0d0d96745b031cc7e4a6a4e59832698476b53a4c
Anubhav Gain <iamanubhavgain@gmail.com>
2024-03-30 00:35:59 +0530

    feat: added terminal color for cli based app

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ use servicehelper::ServiceHelper;
 async fn main() {
     let args: Vec<String> = env::args().collect();
     let print_output = args.contains(&String::from("--print"));
-    
+
     let log_level = if print_output { "info" } else { "warn" };
-    
+
     Builder::from_env(Env::default().default_filter_or(log_level))
         .target(env_logger::Target::default())
         .init();
--- src/servicehelper.rs
+++ src/servicehelper.rs
@@ -1,9 +1,5 @@
 use std::process::Command;
-use serde_json::Value;
-use log::info;
-use tokio::sync::mpsc::{self, Receiver};
-use tokio::time::{interval, Duration};
-use serde_yaml; // Required for YAML serialization
+use ansi_term::Color;
 
 pub struct ServiceHelper {
     pub osquery_prev_status: String,
@@ -13,87 +9,118 @@ pub struct ServiceHelper {
 
 impl ServiceHelper {
     pub async fn run_service_check_timer(&mut self) {
-        let (tx, mut rx): (tokio::sync::mpsc::Sender<Value>, Receiver<Value>) = mpsc::channel(100);
+        let osquery_installed = self.is_osquery_installed();
+        let wazuh_installed = self.is_wazuh_installed();
+        let clamav_installed = self.is_clamav_installed();
 
-        tokio::spawn(async move {
-            let mut osquery_prev_status = String::new();
-            let mut wazuh_prev_status = String::new();
-            let mut clamav_prev_status = String::new();
+        loop {
+            let osquery_status = get_service_status("osqueryd").await;
+            let wazuh_status = get_service_status("wazuh-agentd").await;
+            let clamav_status = get_service_status("clamav-clamonacc").await;
 
-            let mut interval = interval(Duration::from_secs(10));
-
-            loop {
-                let osquery_status = get_service_status("osqueryd").await;
-                let wazuh_status = get_service_status("wazuh-agentd").await;
-                let clamav_status = get_service_status("clamav-clamonacc").await;
-
-                if osquery_status != osquery_prev_status ||
-                   wazuh_status != wazuh_prev_status ||
-                   clamav_status != clamav_prev_status {
-
-                    osquery_prev_status = osquery_status.clone();
-                    wazuh_prev_status = wazuh_status.clone();
-                    clamav_prev_status = clamav_status.clone();
-
-                    let menu_item_data = get_menu_item_data(
-                        true, // Assuming services are installed for simplicity; replace with actual checks
-                        true,
-                        true,
-                        osquery_status,
-                        wazuh_status,
-                        clamav_status,
-                    ).await;
-
-                    if let Err(e) = tx.send(menu_item_data).await {
-                        eprintln!("Error sending update through channel: {}", e);
-                    }
-                }
-
-                interval.tick().await;
+            let mut osquery_string = String::new();
+            if !osquery_installed {
+                osquery_string.push_str("not ");
+                osquery_string.push_str(&Color::Red.paint("installed").to_string());
+                osquery_string.push_str(" ");
             }
-        });
 
-        while let Some(update) = rx.recv().await {
-            let yaml_string = serde_yaml::to_string(&update).expect("Failed to convert to YAML");
-            info!("{}", yaml_string);
+            let mut wazuh_string = String::new();
+            if !wazuh_installed {
+                wazuh_string.push_str("not ");
+                wazuh_string.push_str(&Color::Red.paint("installed").to_string());
+                wazuh_string.push_str(" ");
+            }
+
+            let mut clamav_string = String::new();
+            if !clamav_installed {
+                clamav_string.push_str("not ");
+                clamav_string.push_str(&Color::Red.paint("installed").to_string());
+                clamav_string.push_str(" ");
+            }
+
+            let menu_item_data = get_menu_item_data(
+                osquery_installed,
+                wazuh_installed,
+                clamav_installed,
+                osquery_status.clone(),
+                wazuh_status.clone(),
+                clamav_status.clone(),
+                osquery_string,
+                wazuh_string,
+                clamav_string,
+            ).await;
+            println!("{}", menu_item_data);
+
+            tokio::time::sleep(std::time::Duration::from_secs(10)).await;
         }
     }
 
-    // Placeholder for actual service installation checks
-    #[allow(dead_code)]
     fn is_osquery_installed(&self) -> bool {
-        true
+        let osquery_paths = ["/usr/bin/osqueryi", "/usr/bin/osqueryctl"];
+        osquery_paths.iter().all(|&path| std::path::Path::new(path).exists())
     }
-    #[allow(dead_code)]
+
     fn is_wazuh_installed(&self) -> bool {
-        true
+        let required_files = [
+            "agent-auth", "manage_agents", "wazuh-agentd", "wazuh-control",
+            "wazuh-execd", "wazuh-logcollector", "wazuh-modulesd", "wazuh-syscheckd"
+        ];
+        required_files.iter().all(|&file| std::path::Path::new("/var/ossec/bin/").join(file).exists())
     }
-    #[allow(dead_code)]
+
     fn is_clamav_installed(&self) -> bool {
-        true
+        let output = Command::new("which")
+            .arg("clamscan")
+            .output()
+            .expect("Failed to execute command");
+
+        output.status.success()
     }
 }
 
-async fn get_menu_item_data(_osquery_installed: bool, _wazuh_installed: bool, _clamav_installed: bool, osquery_status: String, wazuh_status: String, clamav_status: String) -> Value {
-    serde_json::json!({
-        "menuItems": [
-            {
-                "text": "User Behavior Analytics",
-                "description": format!("osquery is installed and {}.", osquery_status),
-                "status": if osquery_status.contains("active") { 2 } else { 0 }
-            },
-            {
-                "text": "Endpoint Detection and Response",
-                "description": format!("Wazuh is installed and {}.", wazuh_status),
-                "status": if wazuh_status.contains("active") { 2 } else { 0 }
-            },
-            {
-                "text": "End-Point Protection",
-                "description": format!("ClamAV is installed and {}.", clamav_status),
-                "status": if clamav_status.contains("active") { 2 } else { 0 }
-            }
-        ]
-    })
+async fn get_menu_item_data(
+    osquery_installed: bool,
+    wazuh_installed: bool,
+    clamav_installed: bool,
+    osquery_status: String,
+    wazuh_status: String,
+    clamav_status: String,
+    osquery_string: String,
+    wazuh_string: String,
+    clamav_string: String,
+) -> String {
+    format!(
+        r#"menuItems:
+  - text: "User Behavior Analytics"
+    description: "{}"
+    status: {}
+  - text: "Endpoint Detection and Response"
+    description: "{}"
+    status: {}
+  - text: "End-Point Protection"
+    description: "{}"
+    status: {}
+"#,
+        format!(
+            "osquery is {}installed and {}.",
+            osquery_string,
+            osquery_status
+        ),
+        if osquery_installed { 0 } else { 1 },
+        format!(
+            "Wazuh is {}installed and {}.",
+            wazuh_string,
+            wazuh_status
+        ),
+        if wazuh_installed { 0 } else { 1 },
+        format!(
+            "ClamAV is {}installed and {}.",
+            clamav_string,
+            clamav_status
+        ),
+        if clamav_installed { 0 } else { 1 },
+    )
 }
 
 async fn get_service_status(service: &str) -> String {
@@ -108,6 +135,6 @@ async fn get_service_status(service: &str) -> String {
     match status.as_str() {
         "active" => "running".to_string(),
         "inactive" => "halted".to_string(),
-        _ => "stopped".to_string(),
+        "activating" | "deactivating" | "failed" | _ => "stopped".to_string(),
     }
 }
