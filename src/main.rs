7dc0fb604a9653dfe61befae13983db2e441d13b
Anubhav Gain <iamanubhavgain@gmail.com>
2024-03-29 12:18:25 +0530

    todo: fix the warning bugs. complied sucessfully

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use env_logger::Env;
-use std::env;
-
 mod servicehelper;
 use servicehelper::ServiceHelper;
+
+use env_logger::Env;
+use std::env;
+use zbus::connection;
 
 #[tokio::main]
 async fn main() {
@@ -18,5 +19,34 @@ async fn main() {
     let service_helper = ServiceHelper;
 
     // Run the service check timer asynchronously
-    service_helper.run_service_check_timer().await;
+    let dbus_server = run_dbus_server(service_helper).await;
+    if let Err(e) = dbus_server {
+        eprintln!("Error: {}", e);
+    }
+}
+
+async fn run_dbus_server(service_helper: ServiceHelper) -> zbus::Result<()> {
+    let greeter = Greeter { service_helper };
+
+    let _conn = connection::Builder::session()?
+        .name("com.atcults.anubhav")?
+        .build()
+        .await?;
+
+        let _object_server = _conn.object_server().at("/com/atcults/anubhav/Greeter", greeter).await?;
+    let mut h = _conn.request_name("com.atcults.anubhav").await?;
+
+    Ok(())
+}
+
+struct Greeter {
+    service_helper: ServiceHelper,
+}
+
+#[zbus::interface(name = "com.atcults.anubhav")]
+impl Greeter {
+    async fn get_menu_items(&self) -> zbus::fdo::Result<String> {
+        let menu_items_data = self.service_helper.get_menu_item_data().await;
+        Ok(menu_items_data.to_string())
+    }
 }
