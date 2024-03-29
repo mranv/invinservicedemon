cedcf091da1e8ecba7456e115ab297a89cafff92
Anubhav Gain <iamanubhavgain@gmail.com>
2024-03-29 19:00:34 +0530

    feat: adding verified system logger into the application

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use env_logger::Env;
 use std::env;
+use env_logger::Builder;
+
 mod servicehelper;
 use servicehelper::ServiceHelper;
 
@@ -11,7 +13,11 @@ async fn main() {
 
     // Initialize logger using env_logger with default filter set to "info"
     let log_level = if print_output { "info" } else { "warn" }; // Adjust log level based on "--print" argument
-    env_logger::Builder::from_env(Env::default().default_filter_or(log_level)).init();
+    
+    // Configure env_logger to log to the syslog
+    Builder::from_env(Env::default().default_filter_or(log_level))
+        .target(env_logger::Target::default()) // Set the target to the default
+        .init();
 
     // Create an instance of ServiceHelper
     let mut service_helper = ServiceHelper {
